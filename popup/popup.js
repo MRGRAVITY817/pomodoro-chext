d5adc77925df9eb3cbe5aa6ff114b42877e767b8
Hoon Wee <mrgravity817@gmail.com>
2022-04-04 18:01:51 +0900

    Add start stop reset

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,35 @@ const addTaskBtn = document.getElementById("add-task-btn");
 
 let tasks = [];
 
+const startTimerBtn = document.getElementById("start-timer-btn");
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? "Pause Timer"
+          : "Start Timer";
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    }
+  );
+});
+
 chrome.storage.sync.get(["tasks"], (res) => {
   tasks = res.tasks ? res.tasks : [];
   renderTasks();
